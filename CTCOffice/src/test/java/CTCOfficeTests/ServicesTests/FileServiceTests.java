2e41b929a83e9f16a303904d0f466b251420c9df
Robert Taylor <rats3g@gmail.com>
2017-11-27 23:21:56 -0500

    Add more tests for FileService - track layout parsing

--- CTCOffice/src/test/java/CTCOfficeTests/ServicesTests/FileServiceTests.java
+++ CTCOffice/src/test/java/CTCOfficeTests/ServicesTests/FileServiceTests.java
@@ -2,6 +2,7 @@ package CTCOfficeTests.ServicesTests;
 
 import CTCOffice.Services.FileService;
 import TrackModel.Models.*;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -9,12 +10,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class FileServiceTests {
     @Test
-    public void ValidStandardBlock_Parses() throws IOException {
+    public void ValidStandardBlock_Parses() {
         // Arrange
         FileService fileService = new FileService();
 
@@ -29,7 +31,7 @@ public class FileServiceTests {
     }
 
     @Test
-    public void ValidStationBlock_Parses() throws IOException {
+    public void ValidStationBlock_Parses() {
         // Arrange
         FileService fileService = new FileService();
 
@@ -44,7 +46,7 @@ public class FileServiceTests {
     }
 
     @Test
-    public void ValidCrossingBlock_Parses() throws IOException {
+    public void ValidCrossingBlock_Parses() {
         // Arrange
         FileService fileService = new FileService();
 
@@ -59,7 +61,7 @@ public class FileServiceTests {
     }
 
     @Test
-    public void ValidSwitchBlock_Parses() throws IOException {
+    public void ValidSwitchBlock_Parses() {
         // Arrange
         FileService fileService = new FileService();
 
@@ -68,6 +70,129 @@ public class FileServiceTests {
 
         // Act
         Switch result = (Switch) fileService.parseBlock(line);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void InvalidLine_ParseFailure() {
+        // Arrange
+        FileService fileService = new FileService();
+
+        String line = "1,blue,switch,0,0.5,2;3,10,1,false,true,40,25,2,3,1";
+        Block expected = null;
+
+        // Act
+        Block result = fileService.parseBlock(line);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void NonInteger_ParseFailure() {
+        // Arrange
+        FileService fileService = new FileService();
+
+        String line = "hi,green,switch,0,0.5,2;3,10,1,false,true,40,25,2,3,1";
+        Block expected = null;
+
+        // Act
+        Block result = fileService.parseBlock(line);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void NonDouble_ParseFailure() {
+        // Arrange
+        FileService fileService = new FileService();
+
+        String line = "1,green,switch,0,Lol,2;3,10,1,false,true,40,25,2,3,1";
+        Block expected = null;
+
+        // Act
+        Block result = fileService.parseBlock(line);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void NegativeSpeedLimit_ParseFailure() {
+        // Arrange
+        FileService fileService = new FileService();
+
+        String line = "3,green,standard,0,0.5,1;2,10,1,false,true,40,-25";
+        Block expected = null;
+
+        // Act
+        Block result = fileService.parseBlock(line);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void NegativeLength_ParseFailure() {
+        // Arrange
+        FileService fileService = new FileService();
+
+        String line = "3,green,standard,0,0.5,1;2,10,1,false,true,-40,25";
+        Block expected = null;
+
+        // Act
+        Block result = fileService.parseBlock(line);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void ValidLayoutWithSingleLine_Parsed() throws IOException {
+        // Arrange
+        FileService fileService = new FileService();
+
+        BufferedReader bufferedReader = Mockito.mock(BufferedReader.class);
+        Mockito.when(bufferedReader.readLine()).thenReturn("3,green,standard,0,0.5,1;2,10,1,false,true,40,25").thenReturn(null);
+
+        List<Block> expected = new ArrayList<>();
+        Block block = new Block(3, Line.GREEN, BlockType.STANDARD, 0, 0.5, new ArrayList<>(Arrays.asList(1, 2)), 10, 1, false, true, 40, 25);
+        expected.add(block);
+
+        // Act
+        List<Block> result = fileService.parseTrackLayout(bufferedReader);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void ValidLayoutWithMultipleLines_Parsed() throws IOException {
+        // Arrange
+        FileService fileService = new FileService();
+
+        BufferedReader bufferedReader = Mockito.mock(BufferedReader.class);
+        Mockito.when(bufferedReader.readLine()).thenReturn("3,green,standard,0,0.5,1;2,10,1,false,true,40,25")
+                .thenReturn("3,green,station,0,0.5,1;2,10,1,false,true,40,25,TestStation")
+                .thenReturn("3,green,crossing,0,0.5,1;2,10,1,false,true,40,25")
+                .thenReturn("1,green,switch,0,0.5,2;3,10,1,false,true,40,25,2,3,1")
+                .thenReturn(null);
+
+        List<Block> expected = new ArrayList<>();
+        Block block1 = new Block(3, Line.GREEN, BlockType.STANDARD, 0, 0.5, new ArrayList<>(Arrays.asList(1, 2)), 10, 1, false, true, 40, 25);
+        Station block2 = new Station(3, Line.GREEN, BlockType.STATION, 0, 0.5, new ArrayList<>(Arrays.asList(1, 2)), 10, 1, false, true, 40, 25, "TestStation");
+        Crossing block3 = new Crossing(3, Line.GREEN, BlockType.CROSSING, 0, 0.5, new ArrayList<>(Arrays.asList(1, 2)), 10, 1, false, true, 40, 25);
+        Switch block4 = new Switch(1, Line.GREEN, BlockType.SWITCH, 0, 0.5, new ArrayList<>(Arrays.asList(2, 3)), 10, 1, false, true, 40, 25, 2, 3, 1);
+        expected.add(block1);
+        expected.add(block2);
+        expected.add(block3);
+        expected.add(block4);
+
+        // Act
+        List<Block> result = fileService.parseTrackLayout(bufferedReader);
 
         // Assert
         assertEquals(expected, result);
