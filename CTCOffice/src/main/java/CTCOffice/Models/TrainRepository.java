1460b1eb9f6f185dc154c15344e2d3cd5d655341
Robert Taylor <rats3g@gmail.com>
2017-11-28 03:27:16 -0500

    Interim changes towards getting working CTC

--- CTCOffice/src/main/java/CTCOffice/Models/TrainRepository.java
+++ CTCOffice/src/main/java/CTCOffice/Models/TrainRepository.java
@@ -7,24 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 
 @Singleton
-public class Repository {
-    private HashMap<Integer, Block> blocks = new HashMap<>();
+public class TrainRepository {
     private HashMap<Integer, Train> trains = new HashMap<>();
-
-    public List<Block> getBlocks() {
-        List<Block> blockList = new ArrayList<>();
-        blockList.addAll(blocks.values());
-
-        return blockList;
-    }
-
-    public Block getBlock(int blockId) {
-        return blocks.get(blockId);
-    }
-
-    public void addBlock(Block block) {
-        blocks.put(block.getBlockId(), block);
-    }
 
     public List<Train> getTrains() {
         List<Train> trainList = new ArrayList<>();
